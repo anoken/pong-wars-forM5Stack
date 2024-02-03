579e5539f321d06f36010afc9d60a373e23b9433
anoken2017 <31365453+anoken@users.noreply.github.com>
2024-02-03 14:07:38 +0900

    Add files via upload

--- src/user_code.cpp
+++ src/user_code.cpp
@@ -1,14 +1,20 @@
 
 #include <M5Unified.h>
+
+// 画像の幅と高さ、四角形のサイズ、四角形の数を定義
 int img_width = 320;
 int img_height = 240;
 int SQUARE_SIZE = 16;
 int numSquaresX = img_width / SQUARE_SIZE + 1;
 int numSquaresY = img_height / SQUARE_SIZE + 1;
 
+// 四角形の位置情報を格納する2次元ベクトル
 std::vector<std::vector<int>> squares;
+
+// M5UnifiedのCanvasオブジェクト
 M5Canvas canvas(&M5.Display);
 
+// ボールの情報を格納する構造体のベクトル
 struct _pong {
     double x;
     double y;
@@ -20,11 +26,13 @@ double vel = SQUARE_SIZE / 2;
 
 std::vector<_pong> pong;
 
+// 乱数生成関数
 double randomNum(double min, double max) {
     int rand_int = rand() % 100;
     double rand_uni = (rand_int / 100.0) * (max - min) + min;
     return rand_uni;
 }
+// 数値の符号を返す関数
 double sign(double A) {
     if(A == 0)
         return 0;
@@ -32,15 +40,17 @@ double sign(double A) {
         return A / abs(A);
 }
 
+// 四角形とボールの衝突判定と反射を処理する関数
 int updateSquareAndBounce(int numSquaresX, int numSquaresY, double x, double y, double &dx, double &dy, int class_num) {
-
     double updatedDx = dx;
     double updatedDy = dy;
+
     // ボールの円周上の複数のポイントをチェックする
     for(double angle = 0; angle < M_PI * 2; angle += M_PI / 4) {
         int checkX = x + cos(angle) * (SQUARE_SIZE / 2);
         int checkY = y + sin(angle) * (SQUARE_SIZE / 2);
 
+        // チェックしたポイントが画面内かどうか確認
         int i = floor(checkX / SQUARE_SIZE);
         int j = floor(checkY / SQUARE_SIZE);
 
@@ -58,8 +68,7 @@ int updateSquareAndBounce(int numSquaresX, int numSquaresY, double x, double y, 
         }
     }
 
-    // ボールがループにはまらないように、バウンドにノイズを加える。
-
+    // ボールがループにはまらないように、バウンドにノイズを加える
     double theta = M_PI / 4 * (1 + randomNum(-0.1, 1));
     dx = sign(updatedDx) * vel * cos(theta);
     dy = sign(updatedDy) * vel * sin(theta);
@@ -67,6 +76,7 @@ int updateSquareAndBounce(int numSquaresX, int numSquaresY, double x, double y, 
     return 0;
 }
 
+// 画面の境界とボールの衝突を判定し、必要に応じて反射させる関数
 int checkBoundaryCollision(int image_width, int img_height, double x, double y, double &dx, double &dy) {
     if(x + dx > image_width - SQUARE_SIZE / 2 || x + dx < SQUARE_SIZE / 2) {
         dx = -dx;
@@ -77,17 +87,19 @@ int checkBoundaryCollision(int image_width, int img_height, double x, double y, 
     return 0;
 }
 
+// M5Stackデバイスの初期化や位置情報の設定を行うセットアップ関数
 void setup(void) {
-
     auto cfg = M5.config();
     M5.begin(cfg);
     canvas.createSprite(320, 240);
 
+    // 四角形の位置情報を初期化
     squares.resize(numSquaresX);
     for(int i = 0; i < numSquaresX; i++) {
         squares[i].resize(numSquaresY);
     }
 
+    // 画面を4つのエリアに分割し、それぞれに異なるクラスの四角形を配置
     int pong_no = 4;
     pong.resize(pong_no);
 
@@ -107,6 +119,7 @@ void setup(void) {
         }
     }
 
+    // ボールの初期位置と速度を設定
     pong[0].x = img_width / 4;
     pong[0].y = img_height / 4;
     pong[0].dx = vel;
@@ -131,13 +144,15 @@ void setup(void) {
     pong[3].dy = vel;
     pong[3].class_num = 3;
 }
-
 void loop(void) {
-
+    // 画面の描画処理を開始
     M5.Display.startWrite();
+
+    // 四角形の描画
     for(int i = 0; i < squares.size(); i++) {
         for(int j = 0; j < squares[i].size(); j++) {
 
+            // 各クラスに応じて四角形を描画
             if(squares[i][j] == 0) {
                 uint16_t color = M5.Lcd.color565(237, 223, 214);
                 canvas.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, color);
@@ -156,8 +171,12 @@ void loop(void) {
             }
         }
     }
+
+    // ボールの描画と更新
     for(int i = 0; i < pong.size(); i++) {
         uint16_t color = M5.Lcd.color565(237, 223, 214);
+
+        // 各クラスに応じてボールの色を設定
         if(pong[i].class_num == 0)
             color = M5.Lcd.color565(200, 100, 81);
         if(pong[i].class_num == 1)
@@ -166,17 +185,29 @@ void loop(void) {
             color = M5.Lcd.color565(00, 139, 171);
         if(pong[i].class_num == 3)
             color = M5.Lcd.color565(103, 193, 173);
+
+        // ボールを描画
         canvas.fillCircle(pong[i].x, pong[i].y, SQUARE_SIZE / 2, color);
+
+        // 四角形との衝突判定と反射、境界との衝突判定
         updateSquareAndBounce(numSquaresX, numSquaresY, pong[i].x, pong[i].y, pong[i].dx, pong[i].dy, pong[i].class_num);
         checkBoundaryCollision(img_width, img_height, pong[i].x, pong[i].y, pong[i].dx, pong[i].dy);
 
+        // ボールの位置を更新
         pong[i].x += pong[i].dx;
         pong[i].y += pong[i].dy;
     }
+
+    // Canvasに描画された内容をスクリーンに転送
     canvas.pushSprite(0, 0);
 
+    // 画面の描画処理を終了
     M5.Display.endWrite();
+
+    // 10ミリ秒待機
     M5.delay(10);
+
+    // M5Stackの状態を更新
     M5.update();
 }
 
